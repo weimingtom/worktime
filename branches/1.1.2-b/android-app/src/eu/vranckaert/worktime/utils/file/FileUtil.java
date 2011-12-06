4c981de747912d9f20c248a27f62883b00b9b4ba
dirkvranckaert <dirkvranckaert@5c578369-1464-59e2-a874-362b38da20de>
2011-12-06 18:59:02 +0000

    Release 1.1.2.b

---
+++ branches/1.1.2-b/android-app/src/eu/vranckaert/worktime/utils/file/FileUtil.java
@@ -0,0 +1,41 @@
+/*
+ *  Copyright 2011 Dirk Vranckaert
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package eu.vranckaert.worktime.utils.file;
+
+import java.io.*;
+import java.nio.channels.FileChannel;
+
+/**
+ * User: DIRK VRANCKAERT
+ * Date: 12/09/11
+ * Time: 16:55
+ */
+public class FileUtil {
+    public static void copyFile(File src, File dest) throws IOException {
+        FileChannel inChannel = new FileInputStream(src).getChannel();
+        FileChannel outChannel = new FileOutputStream(dest).getChannel();
+        try {
+            inChannel.transferTo(0, inChannel.size(), outChannel);
+        } finally {
+            if (inChannel != null) {
+                inChannel.close();
+            }
+            if (outChannel != null) {
+                outChannel.close();
+            }
+        }
+    }
+}
