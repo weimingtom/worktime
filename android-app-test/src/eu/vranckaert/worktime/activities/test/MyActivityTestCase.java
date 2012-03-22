814c6f28fb19d123f2d4221b54b1b0db9f151f3f
dirkvranckaert <dirkvranckaert@5c578369-1464-59e2-a874-362b38da20de>
2012-03-22 13:53:47 +0000

    Fixing the unit tests + including a better solution for making the tests independent when it comes to the DB! Now cleaning up the DB in a better way than just deleting the DB file...

--- android-app-test/src/eu/vranckaert/worktime/activities/test/MyActivityTestCase.java
+++ android-app-test/src/eu/vranckaert/worktime/activities/test/MyActivityTestCase.java
@@ -17,17 +17,24 @@ package eu.vranckaert.worktime.activities.test;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.test.ActivityInstrumentationTestCase2;
 import android.util.Log;
+import com.j256.ormlite.support.ConnectionSource;
 import com.jayway.android.robotium.solo.Solo;
 import eu.vranckaert.worktime.constants.Constants;
+import eu.vranckaert.worktime.dao.utils.DaoConstants;
+import eu.vranckaert.worktime.dao.utils.DatabaseHelper;
 import eu.vranckaert.worktime.testutils.ScreenshotUtil;
 import eu.vranckaert.worktime.utils.preferences.Preferences;
 
 import java.io.File;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -39,6 +46,8 @@ public class MyActivityTestCase<V extends Activity> extends ActivityInstrumentat
     private final String LOG_TAG = MyActivityTestCase.class.getSimpleName();
     private Class testClass;
     private int screenshotCountNumber;
+    
+    private Intent customIntent = null;
 
     protected Solo solo;
 
@@ -46,17 +55,11 @@ public class MyActivityTestCase<V extends Activity> extends ActivityInstrumentat
         super("eu.vranckaert.worktime", clazz);
         this.testClass = clazz;
     }
-    
-    private String getFullTestedClassPackageName() {
-        String name = "";
-        
-        name += testClass.getPackage().getName();
-        name += ".";
-        name += testClass.getSimpleName();
-        
-        return name;
-    }
-    
+
+    /**
+     * Gets you the full package and class name of this test.
+     * @return The full package and class name of this test.
+     */
     private String getFullTestClassName() {
         return this.getClass().getName();
     }
@@ -65,6 +68,8 @@ public class MyActivityTestCase<V extends Activity> extends ActivityInstrumentat
     protected void setUp() throws Exception {
         beforeTestBeforeActivityLaunch();
         super.setUp();
+        launchActivity();
+        removeAllPreferences();
         solo = new Solo(getInstrumentation(), getActivity());
         beforeTestAfterActivityLaunch();
     }
@@ -73,6 +78,7 @@ public class MyActivityTestCase<V extends Activity> extends ActivityInstrumentat
     protected void runTest() throws Throwable {
         super.runTest();
         afterTest();
+        getActivity().finish();
     }
 
     @Override
@@ -80,16 +86,38 @@ public class MyActivityTestCase<V extends Activity> extends ActivityInstrumentat
         solo.finishOpenedActivities();
     }
 
+    /**
+     * Everything to be done before the test starts, AND before the activity is launched. In here no calls can be done
+     * to the {@link android.test.ActivityInstrumentationTestCase2#getActivity()} method. 
+     */
     private void beforeTestBeforeActivityLaunch() {
-        cleanUpDatabase();
-        removeAllPreferences();
+        String[] tables = {"commentHistory", "project", "task", "timeregistration"};
+        cleanUpDatabase(Arrays.asList(tables));
     }
 
+    /**
+     * Launches the activity. If the {@link MyActivityTestCase#customIntent} is empty it will just start the activity.
+     * If the intent is available it will use start the activity with the provided intent!
+     */
+    private void launchActivity() {
+        if (customIntent != null) {
+            setActivityIntent(customIntent);
+        }
+        getActivity();
+    }
+
+    /**
+     * Everything to be done before the test starts, but after the activity is launched. In here you can call the 
+     * {@link android.test.ActivityInstrumentationTestCase2#getActivity()} method.
+     */
     private void beforeTestAfterActivityLaunch() {
         screenshotCountNumber = -1;
         takeScreenshot();
     }
 
+    /**
+     * Everything to be done after test
+     */
     private void afterTest() {
         if (solo.getViews().size() > 0) {
             try {
@@ -106,11 +134,14 @@ public class MyActivityTestCase<V extends Activity> extends ActivityInstrumentat
         } else {
             Log.w(LOG_TAG, "Solo does not contain any views, not creating a screenshot!");
         }
-        removeAllPreferences();
-        cleanUpDatabase();
     }
 
-    protected String getNextScreenshotCountNumber() throws Exception {
+    /**
+     * Get the next number for the next screenshot.
+     * @return The number of the next screenshot in {@link String} format.
+     * @throws Exception An exception if you are exceeding the limit of 9999 screenshots.
+     */
+    private String getNextScreenshotCountNumber() throws Exception {
         screenshotCountNumber++;
 
         if (screenshotCountNumber < 10) {
@@ -126,10 +157,17 @@ public class MyActivityTestCase<V extends Activity> extends ActivityInstrumentat
         throw new Exception("No more than 9999 screenshots are allowed!");
     }
 
+    /**
+     * Get the number of the last screenshot to be taken of the app.
+     * @return The number of the last screenshot (9999).
+     */
     private String getLastScreenshotCountNumber() {
         return "9999";
     }
 
+    /**
+     * Removes all the preferences
+     */
     protected void removeAllPreferences() {
         List<String> preferenceKeys = new ArrayList<String>();
 
@@ -146,6 +184,46 @@ public class MyActivityTestCase<V extends Activity> extends ActivityInstrumentat
         }
     }
 
+    /**
+     * Removes the database
+     */
+    private void cleanUpDatabase(List<String> dbTables) {
+        Log.i(LOG_TAG, "Preparing to clean up database...");
+        DatabaseHelper dbHelper = new DatabaseHelper(getInstrumentation().getTargetContext());
+        ConnectionSource cs = dbHelper.getConnectionSource();
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        
+        Log.i(LOG_TAG, "Dropping all tables");
+        for (String table : dbTables) {
+            db.execSQL("DROP TABLE IF EXISTS " + table);
+        }
+
+        Log.i(LOG_TAG, "Executing the onCreate(..)");
+        dbHelper.onCreate(db, cs);
+        
+        Log.i(LOG_TAG, "Verifying the data...");
+        for (String table : dbTables) {
+            Cursor c = db.query(table, new String[]{"id"}, null, null, null, null, null);
+            int count = c.getCount();
+            if (count != 1 && (table.equals("project") || table.equals("task"))) {
+                dbHelper.close();
+                Log.e(LOG_TAG, "We should have 1 record for table " + table + " after cleanup but we found " + count + " record(s)");
+                throw new RuntimeException("Error during cleanup of DB, exactly one record should be present for table " + table + " but we found " + count + " record(s)");
+            } else if (count != 0 && !(table.equals("project") || table.equals("task"))) {
+                dbHelper.close();
+                Log.e(LOG_TAG, "We should have 0 records for table " + table + " after cleanup but we found " + count + " record(s)");
+                throw new RuntimeException("Error during cleanup of DB, no records should be present for table " + table + " but we found " + count + " record(s)");
+            }
+        }
+
+        Log.i(LOG_TAG, "The database has been cleaned!");
+        dbHelper.close();
+    }
+    
+    /*
+     * PROTECTED METHODS
+     */
+
     protected void takeScreenshot() {
         if (solo.getViews().size() > 0) {
             try {
@@ -161,17 +239,6 @@ public class MyActivityTestCase<V extends Activity> extends ActivityInstrumentat
             }
         } else {
             Log.w(LOG_TAG, "Solo does not contain any views, not creating a screenshot!");
-        }
-    }
-    
-    private void cleanUpDatabase() {
-        File dir = new File("/data/data/eu.vranckaert.worktime/databases/");
-        if (!dir.exists()) {
-            return;
-        }
-        File files [] = dir.listFiles();
-        for (File file : files) {
-            file.delete();
         }
     }
     
@@ -195,5 +262,9 @@ public class MyActivityTestCase<V extends Activity> extends ActivityInstrumentat
             editor.putString(key, value.toString());
         }
         editor.commit();
+    }
+
+    protected void setCustomIntent(Intent customIntent) {
+        customIntent = customIntent;
     }
 }
---
+++ android-app-test/src/eu/vranckaert/worktime/activities/timeregistrations/TimeRegistrationsActivityTest.java
@@ -0,0 +1,115 @@
+/*
+ *  Copyright 2012 Dirk Vranckaert
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package eu.vranckaert.worktime.activities.timeregistrations;
+
+import android.app.ListActivity;
+import eu.vranckaert.worktime.R;
+import eu.vranckaert.worktime.activities.HomeActivity;
+import eu.vranckaert.worktime.activities.test.MyActivityTestCase;
+import eu.vranckaert.worktime.activities.timeregistrations.TimeRegistrationsActivity;
+import eu.vranckaert.worktime.activities.widget.StopTimeRegistrationActivity;
+import eu.vranckaert.worktime.constants.Constants;
+import eu.vranckaert.worktime.testutils.TestConstants;
+
+/**
+ * User: DIRK VRANCKAERT
+ * Date: 20/03/12
+ * Time: 9:41
+ */
+public class TimeRegistrationsActivityTest extends MyActivityTestCase<TimeRegistrationsActivity> {
+    public TimeRegistrationsActivityTest() {
+        super(TimeRegistrationsActivity.class);
+    }
+
+    public void testGoHome() {
+        solo.assertCurrentActivity("The time registrations activity is expected", TimeRegistrationsActivity.class);
+        solo.clickOnImageButton(0);
+        solo.assertCurrentActivity("The home activity is expected", HomeActivity.class);
+    }
+
+    public void testDefaultEmptyList() {
+        int trCount = ((ListActivity)solo.getCurrentActivity()).getListView().getAdapter().getCount();
+        assertEquals("No TR's are expected!", 0, trCount);
+    }
+
+    public void testPunchBar() {
+        // Enable the punch-in bar for all screens...
+        setPreference(Constants.Preferences.Keys.TIME_REGISTRATION_PUNCH_BAR_ENABLED_ON_ALL_SCREENS, Boolean.TRUE);
+        reloadActivity();
+
+        int trCount = ((ListActivity)solo.getCurrentActivity()).getListView().getAdapter().getCount();
+        assertEquals("No TR's are expected!", 0, trCount);
+
+        // Start a new time registration
+        solo.clickOnView(getActivity().findViewById(R.id.punchBarActionId));
+        solo.waitForDialogToClose(TestConstants.Time.THIRTY_SECONDS);
+        solo.clickOnText(getActivity().getString(R.string.default_project_name), 0, true);
+        solo.clickOnText(getActivity().getString(R.string.default_task_name), 0, true);
+
+        solo.waitForDialogToClose(TestConstants.Time.THIRTY_SECONDS);
+        solo.waitForActivity(TimeRegistrationsActivity.class.getSimpleName());
+        trCount = ((ListActivity)solo.getCurrentActivity()).getListView().getAdapter().getCount();
+        assertEquals("Expected just one time registration to be available!", 1, trCount);
+
+        takeScreenshot();
+
+        solo.clickLongInList(0);
+        solo.waitForDialogToClose(TestConstants.Time.SIXTY_SECONDS);
+        assertTrue(solo.searchText(getActivity().getString(R.string.lbl_registrations_menu_details), 1, true));
+        assertTrue(solo.searchText(getActivity().getString(R.string.lbl_registrations_menu_edit_start), 1, true));
+        assertTrue(solo.searchText(getActivity().getString(R.string.lbl_registrations_menu_split), 1, true));
+        assertTrue(solo.searchText(getActivity().getString(R.string.lbl_registrations_menu_add_comment), 1, true));
+        assertTrue(solo.searchText(getActivity().getString(R.string.lbl_registrations_menu_edit_project_task), 1, true));
+        assertTrue(solo.searchText(getActivity().getString(R.string.lbl_registrations_menu_delete), 1, true));
+        solo.goBack();
+
+
+        // End the time registration
+        solo.clickOnView(getActivity().findViewById(R.id.punchBarActionId));
+        solo.waitForActivity(StopTimeRegistrationActivity.class.getSimpleName());
+        solo.clickOnButton(getActivity().getString(R.string.btn_widget_stop));
+        solo.waitForActivity(TimeRegistrationsActivity.class.getSimpleName());
+        solo.waitForDialogToClose(TestConstants.Time.THIRTY_SECONDS);
+        trCount = ((ListActivity)solo.getCurrentActivity()).getListView().getAdapter().getCount();
+        assertEquals("Expected just one time registration to be available!", 1, trCount);
+        assertTrue(solo.searchText(getActivity().getString(R.string.home_comp_start_stop_time_registration_no_ongoing), 0, true));
+
+        takeScreenshot();
+
+        solo.clickLongInList(0);
+        solo.waitForDialogToClose(TestConstants.Time.THIRTY_SECONDS);
+        assertTrue(solo.searchText(getActivity().getString(R.string.lbl_registrations_menu_details), 1, true));
+        assertTrue(solo.searchText(getActivity().getString(R.string.lbl_registrations_menu_edit_start), 1, true));
+        assertTrue(solo.searchText(getActivity().getString(R.string.lbl_registrations_menu_edit_end), 1, true));
+        assertTrue(solo.searchText(getActivity().getString(R.string.lbl_registrations_menu_split), 1, true));
+        assertTrue(solo.searchText(getActivity().getString(R.string.lbl_registrations_menu_add_comment), 1, true));
+        assertTrue(solo.searchText(getActivity().getString(R.string.lbl_registration_menu_restart), 1, true));
+        assertTrue(solo.searchText(getActivity().getString(R.string.lbl_registrations_menu_edit_project_task), 1, true));
+        assertTrue(solo.searchText(getActivity().getString(R.string.lbl_registrations_menu_delete), 1, true));
+    }
+
+    private void reloadActivity() {
+        // Go to the home-activity
+        solo.clickOnImageButton(0);
+        solo.waitForActivity(HomeActivity.class.getSimpleName());
+        solo.assertCurrentActivity("The home activity is expected", HomeActivity.class);
+
+        // Open the time registrations activity
+        solo.clickOnButton(0);
+        solo.waitForActivity(TimeRegistrationsActivity.class.getSimpleName());
+        solo.assertCurrentActivity("The time registrations activity is expected", TimeRegistrationsActivity.class);
+    }
+}
