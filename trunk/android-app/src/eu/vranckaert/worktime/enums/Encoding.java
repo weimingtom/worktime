cc9c95d1a29f95ae535e8c311d48f2ec72fc8ab5
dirkvranckaert <dirkvranckaert@5c578369-1464-59e2-a874-362b38da20de>
2012-03-30 11:47:40 +0000

    For issue 100 when applying the BOM for the export we added three spaces, then the we override the first 3 characters with the BOM (3 if it's UTF-8!) and that array of bytes we write to the FileOutputStream. Now we first write the bytes (doesn't matter how much) to the FileOutputStream, then we write the bytes of the text to the FileOutputStream.

--- trunk/android-app/src/eu/vranckaert/worktime/enums/Encoding.java
+++ trunk/android-app/src/eu/vranckaert/worktime/enums/Encoding.java
@@ -22,25 +22,29 @@ import java.nio.charset.Charset;
  * @author Dirk Vranckaert
  */
 public enum Encoding {
-    UTF_8("UTF-8", new Byte[] {(byte) 0xEF, (byte) 0xBB, (byte) 0xBF}),
-    UTF_16_BE("UTF-16BE", new Byte[] {(byte) 0xFE, (byte) 0xFF}),
-    UTF_16_LE("UTF-16LE", new Byte[] {(byte) 0xFF, (byte) 0xFE});
+    UTF_8("UTF-8", new byte[] {(byte) 0xEF, (byte) 0xBB, (byte) 0xBF}),
+    UTF_16_BE("UTF-16BE", new byte[] {(byte) 0xFE, (byte) 0xFF}),
+    UTF_16_LE("UTF-16LE", new byte[] {(byte) 0xFF, (byte) 0xFE});
     
     private String encoding;
-    private Byte[] byteOrderMarker;
+    private byte[] byteOrderMarker;
     
-    Encoding(String encoding, Byte[] byteOrderMarker) {
+    Encoding(String encoding, byte[] byteOrderMarker) {
 	this.encoding = encoding;
 	this.byteOrderMarker = byteOrderMarker;
+    }
+    
+    public String getEncoding() {
+	return encoding;
+    }
+    
+    public byte[] getByteOrderMarker() {
+	return byteOrderMarker;
     }
     
     public byte[] encodeString(String text) {
 	Charset charset = Charset.forName(encoding);
 	ByteBuffer b = charset.encode(text);
-	for (int i=0; i<byteOrderMarker.length; i++) {
-	    byte bomByte = byteOrderMarker[i];
-	    b.put(i, bomByte);
-	}
 	return b.array();
     }
 }
