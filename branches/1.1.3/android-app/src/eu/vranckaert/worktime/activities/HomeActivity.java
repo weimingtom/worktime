d35d5dcbbe4c404a223c78c50e31390371bc24a0
dirkvranckaert <dirkvranckaert@5c578369-1464-59e2-a874-362b38da20de>
2012-04-12 12:36:48 +0000

    Creating branch for production release 151/1.1.3

---
+++ branches/1.1.3/android-app/src/eu/vranckaert/worktime/activities/HomeActivity.java
@@ -0,0 +1,134 @@
+/*
+ *  Copyright 2011 Dirk Vranckaert
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package eu.vranckaert.worktime.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import com.google.inject.Inject;
+import eu.vranckaert.worktime.R;
+import eu.vranckaert.worktime.activities.about.AboutActivity;
+import eu.vranckaert.worktime.activities.preferences.PreferencesActivity;
+import eu.vranckaert.worktime.activities.preferences.PreferencesICSActivity;
+import eu.vranckaert.worktime.activities.projects.ManageProjectsActivity;
+import eu.vranckaert.worktime.activities.reporting.ReportingCriteriaActivity;
+import eu.vranckaert.worktime.activities.timeregistrations.TimeRegistrationsActivity;
+import eu.vranckaert.worktime.activities.widget.StartTimeRegistrationActivity;
+import eu.vranckaert.worktime.activities.widget.StopTimeRegistrationActivity;
+import eu.vranckaert.worktime.constants.Constants;
+import eu.vranckaert.worktime.constants.OSContants;
+import eu.vranckaert.worktime.constants.TrackerConstants;
+import eu.vranckaert.worktime.model.TimeRegistration;
+import eu.vranckaert.worktime.service.CommentHistoryService;
+import eu.vranckaert.worktime.service.ProjectService;
+import eu.vranckaert.worktime.service.TaskService;
+import eu.vranckaert.worktime.service.TimeRegistrationService;
+import eu.vranckaert.worktime.utils.context.ContextUtils;
+import eu.vranckaert.worktime.utils.preferences.Preferences;
+import eu.vranckaert.worktime.utils.punchbar.PunchBarUtil;
+import eu.vranckaert.worktime.utils.tracker.AnalyticsTracker;
+import roboguice.activity.GuiceActivity;
+
+public class HomeActivity extends GuiceActivity {
+    private static final String LOG_TAG = HomeActivity.class.getSimpleName();
+
+    @Inject
+    private CommentHistoryService commentHistoryService;
+    
+    @Inject
+    private TimeRegistrationService timeRegistrationService;
+
+    @Inject
+    private TaskService taskService;
+
+    @Inject
+    private ProjectService projectService;
+
+    private AnalyticsTracker tracker;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        tracker = AnalyticsTracker.getInstance(getApplicationContext());
+        tracker.trackPageView(TrackerConstants.PageView.HOME_ACTIVITY);
+
+        initiateDatabase();
+    }
+
+    private void initiateDatabase() {
+        // By default the database is only initiated upon first call (so when tyring to load tasks/projects or time
+        // registrations. We force the creation of the database by loading the last comment.
+        commentHistoryService.findLastComment();
+    }
+
+    private void launchActivity(Class activity) {
+        Intent intent = new Intent(getApplicationContext(), activity);
+        startActivity(intent);
+    }
+
+    public void onTimeRegistrationsClick(View view) {
+        launchActivity(TimeRegistrationsActivity.class);
+    }
+
+    public void onProjectsClick(View view) {
+        launchActivity(ManageProjectsActivity.class);
+    }
+
+    public void onPreferencesClick(View view) {
+        launchActivity(PreferencesActivity.class);
+//        if (ContextUtils.getAndroidApiVersion() < OSContants.API.HONEYCOMB_3_0) {
+//            launchActivity(PreferencesActivity.class);
+//        } else {
+//            launchActivity(PreferencesICSActivity.class);
+//        }
+    }
+
+    public void onReportingClick(View view) {
+        launchActivity(ReportingCriteriaActivity.class);
+    }
+
+    public void onAboutClick(View view) {
+        launchActivity(AboutActivity.class);
+    }
+    
+    public void onPunchButtonClick(View view) {
+        PunchBarUtil.onPunchButtonClick(HomeActivity.this, timeRegistrationService);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        PunchBarUtil.configurePunchBar(HomeActivity.this, timeRegistrationService, taskService, projectService);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Constants.IntentRequestCodes.PUNCH_BAR_START_TIME_REGISTRATION
+                || requestCode == Constants.IntentRequestCodes.PUNCH_BAR_END_TIME_REGISTRATION) {
+            PunchBarUtil.configurePunchBar(HomeActivity.this, timeRegistrationService, taskService, projectService);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        tracker.stopSession();
+    }
+}
---
+++ branches/1.1.3/android-app/src/eu/vranckaert/worktime/activities/widget/SelectProjectActivity.java
@@ -0,0 +1,115 @@
+/*
+ *  Copyright 2011 Dirk Vranckaert
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package eu.vranckaert.worktime.activities.widget;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import com.google.inject.Inject;
+import eu.vranckaert.worktime.R;
+import eu.vranckaert.worktime.comparators.project.ProjectByNameComparator;
+import eu.vranckaert.worktime.constants.Constants;
+import eu.vranckaert.worktime.model.Project;
+import eu.vranckaert.worktime.service.ProjectService;
+import eu.vranckaert.worktime.service.WidgetService;
+import eu.vranckaert.worktime.utils.preferences.Preferences;
+import eu.vranckaert.worktime.utils.string.StringUtils;
+import roboguice.activity.GuiceActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * User: DIRK VRANCKAERT
+ * Date: 02/03/11
+ * Time: 20:56
+ */
+public class SelectProjectActivity extends GuiceActivity {
+    @Inject
+    private ProjectService projectService;
+
+    @Inject
+    private WidgetService widgetService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        showDialog(Constants.Dialog.CHOOSE_SELECTED_PROJECT);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        Dialog dialog = null;
+
+        switch (id) {
+            case Constants.Dialog.CHOOSE_SELECTED_PROJECT: {
+                //Find all projects and sort by name
+                List<Project> selectableProjects;
+                if (Preferences.getSelectProjectHideFinished(SelectProjectActivity.this)) {
+                    selectableProjects = projectService.findUnfinishedProjects();
+                } else {
+                    selectableProjects = projectService.findAll();
+                }
+                Collections.sort(selectableProjects, new ProjectByNameComparator());
+                final List<Project> availableProjects = selectableProjects;
+
+                Project selectedProject = projectService.getSelectedProject();
+                int selectedProjectIndex = -1;
+
+                List<String> projects = new ArrayList<String>();
+                for (int i=0; i<availableProjects.size(); i++) {
+                    Project project = availableProjects.get(i);
+
+                    if (selectedProject.getId() == project.getId()) {
+                        selectedProjectIndex = i;
+                    }
+
+                    projects.add(project.getName());
+                }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(R.string.lbl_widget_title_select_project)
+                       .setSingleChoiceItems(
+                               StringUtils.convertListToArray(projects),
+                               selectedProjectIndex,
+                               new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialogInterface, int index) {
+                                        Project newSelectedProject = availableProjects.get(index);
+                                        projectService.setSelectedProject(newSelectedProject);
+                                        widgetService.updateWidget(SelectProjectActivity.this);
+                                        setResult(RESULT_OK);
+                                        SelectProjectActivity.this.finish();
+                                    }
+                               }
+                       )
+                       .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                           public void onCancel(DialogInterface dialogInterface) {
+                               widgetService.updateWidget(SelectProjectActivity.this);
+                               setResult(RESULT_CANCELED);
+                               SelectProjectActivity.this.finish();
+                           }
+                       });
+                dialog = builder.create();
+                break;
+            }
+        }
+
+        return dialog;
+    }
+}
