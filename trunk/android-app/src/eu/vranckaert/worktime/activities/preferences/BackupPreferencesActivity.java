310196061e57ba28dbdb4fd9e1ab6d0d52396860
dirkvranckaert <dirkvranckaert@5c578369-1464-59e2-a874-362b38da20de>
2012-08-28 12:57:08 +0000

    issue 140 - Choose backup location in predefined list

--- trunk/android-app/src/eu/vranckaert/worktime/activities/preferences/BackupPreferencesActivity.java
+++ trunk/android-app/src/eu/vranckaert/worktime/activities/preferences/BackupPreferencesActivity.java
@@ -16,10 +16,21 @@
 
 package eu.vranckaert.worktime.activities.preferences;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.widget.Toast;
 import eu.vranckaert.worktime.R;
+import eu.vranckaert.worktime.constants.Constants;
 import eu.vranckaert.worktime.constants.TrackerConstants;
+import eu.vranckaert.worktime.exceptions.file.ExternalStorageNotAvailableException;
+import eu.vranckaert.worktime.exceptions.file.ExternalStorageNotWritableException;
 import eu.vranckaert.worktime.utils.activity.GenericPreferencesActivity;
+import eu.vranckaert.worktime.utils.file.DirectoryPicker;
+import eu.vranckaert.worktime.utils.file.FileUtil;
+import eu.vranckaert.worktime.utils.preferences.Preferences;
+
+import java.io.File;
 
 /**
  * User: DIRK VRANCKAERT
@@ -32,6 +43,42 @@ public class BackupPreferencesActivity extends GenericPreferencesActivity {
         super.onCreate(savedInstanceState);
 
         setTitle(R.string.pref_backup_category_title);
+
+        Preference backupLocation = (Preference) getPreferenceScreen().findPreference(Constants.Preferences.Keys.BACKUP_LOCATION);
+        backupLocation.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                try {
+                    FileUtil.checkExternalStorageState();
+                } catch (ExternalStorageNotWritableException e) {
+                    // Not a problem in this case...
+                } catch (ExternalStorageNotAvailableException e) {
+                    Toast.makeText(BackupPreferencesActivity.this, R.string.warning_msg_external_storage_unavailable, Toast.LENGTH_LONG).show();
+                    return false;
+                }
+
+                File currentBackupLocation = Preferences.getBackupLocation(BackupPreferencesActivity.this);
+                if (!currentBackupLocation.exists() || !currentBackupLocation.isDirectory()) {
+                    currentBackupLocation = FileUtil.getDefaultBackupDir();
+                }
+
+                Intent directoryPickerIntent = new Intent(BackupPreferencesActivity.this, DirectoryPicker.class);
+                directoryPickerIntent.putExtra(DirectoryPicker.START_DIR, currentBackupLocation.getAbsolutePath());
+                directoryPickerIntent.putExtra(DirectoryPicker.ONLY_DIRS, false);
+                directoryPickerIntent.putExtra(DirectoryPicker.ALLOW_CREATE_DIR, true);
+                startActivityForResult(directoryPickerIntent, DirectoryPicker.PICK_DIRECTORY);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == DirectoryPicker.PICK_DIRECTORY && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            File selectedBackupLocation = (File) extras.get(DirectoryPicker.CHOSEN_DIRECTORY);
+            Preferences.setBackupLocation(this, selectedBackupLocation);
+        }
     }
 
     @Override
