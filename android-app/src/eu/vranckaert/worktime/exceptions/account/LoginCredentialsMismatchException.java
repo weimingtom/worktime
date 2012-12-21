f92165306aa9b3562051757c09cab166ab71e3e3
vranckaertci <vranckaertci@5c578369-1464-59e2-a874-362b38da20de>
2012-12-21 15:47:34 +0000

    Creating branch for production release {releasedVersion}

--- android-app/src/eu/vranckaert/worktime/exceptions/account/LoginCredentialsMismatchException.java
+++
@@ -1,9 +0,0 @@
-package eu.vranckaert.worktime.exceptions.account;
-
-/**
- * User: Dirk Vranckaert
- * Date: 13/12/12
- * Time: 15:29
- */
-public class LoginCredentialsMismatchException extends Exception {
-}
