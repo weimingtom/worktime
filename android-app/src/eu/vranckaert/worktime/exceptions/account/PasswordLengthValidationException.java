2b5add21189d3231b61e71b72feb7568da2bbf0d
vranckaertci <vranckaertci@5c578369-1464-59e2-a874-362b38da20de>
2013-01-14 13:40:32 +0000

    Creating branch for production release 10

--- android-app/src/eu/vranckaert/worktime/exceptions/account/PasswordLengthValidationException.java
+++
@@ -1,9 +0,0 @@
-package eu.vranckaert.worktime.exceptions.account;
-
-/**
- * User: Dirk Vranckaert
- * Date: 19/12/12
- * Time: 14:42
- */
-public class PasswordLengthValidationException extends Exception {
-}
