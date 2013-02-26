9230ea687b4f553bef6cef57d4d4ac8466845358
vranckaertci <vranckaertci@5c578369-1464-59e2-a874-362b38da20de>
2013-02-26 20:06:51 +0000

    Creating branch for production release 16

---
+++ branches/web-app-16/web-app/src/eu/vranckaert/worktime/json/endpoint/impl/SetupEndpoint.java
@@ -0,0 +1,41 @@
+package eu.vranckaert.worktime.json.endpoint.impl;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.inject.Inject;
+
+import eu.vranckaert.worktime.model.Service;
+import eu.vranckaert.worktime.model.ServicePlatform;
+import eu.vranckaert.worktime.security.dao.ServiceDao;
+import eu.vranckaert.worktime.security.utils.KeyGenerator;
+
+@Path("setup")
+public class SetupEndpoint {
+	@Inject
+	private ServiceDao serviceDao;
+	
+	@GET
+	@Path("hello")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayHello() {
+		return "Hello World";
+	}
+	
+//	@GET
+//	@Path("setupService")
+//	@Produces(MediaType.TEXT_PLAIN)
+//	public String setupService() {
+//		// Create a setup service...
+//		Service serviceTest = new Service();
+//		serviceTest.setAppName("WorkTime for Android");
+//		serviceTest.setContact("dirkvranckaert@gmail.com");
+//		serviceTest.setPlatform(ServicePlatform.ANDROID);
+//		serviceTest.setServiceKey(KeyGenerator.getNewKey());
+//		serviceDao.persist(serviceTest);
+//		
+//		return "Service created!";
+//	}
+}
