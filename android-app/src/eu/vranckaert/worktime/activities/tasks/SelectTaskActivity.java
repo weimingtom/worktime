68cd40149ab3c86ce20c9e594dc14ca18db7488f
dirkvranckaert <dirkvranckaert@5c578369-1464-59e2-a874-362b38da20de>
2013-03-22 19:23:08 +0000

    issue 206 - Added the add project and task feature in the project and task selection when starting a TR and when modifying the project and task of a TR

--- android-app/src/eu/vranckaert/worktime/activities/tasks/SelectTaskActivity.java
+++ android-app/src/eu/vranckaert/worktime/activities/tasks/SelectTaskActivity.java
@@ -16,11 +16,11 @@
 package eu.vranckaert.worktime.activities.tasks;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import com.google.inject.Inject;
+import com.google.inject.internal.Nullable;
 import eu.vranckaert.worktime.R;
 import eu.vranckaert.worktime.comparators.task.TaskByNameComparator;
 import eu.vranckaert.worktime.constants.Constants;
@@ -32,10 +32,8 @@ import eu.vranckaert.worktime.service.ui.WidgetService;
 import eu.vranckaert.worktime.utils.preferences.Preferences;
 import eu.vranckaert.worktime.utils.string.StringUtils;
 import eu.vranckaert.worktime.utils.view.actionbar.synclock.SyncLockedGuiceActivity;
-import roboguice.activity.RoboActivity;
 import roboguice.inject.InjectExtra;
 
-import com.google.inject.internal.Nullable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -68,12 +66,31 @@ public class SelectTaskActivity extends SyncLockedGuiceActivity {
     @InjectExtra(value = Constants.Extras.UPDATE_WIDGET, optional = true)
     private boolean updateWidget = false;
 
-    private List<Task> selectableTasks;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        List<Task> selectableTasks = loadSelectableTasks();
+
+
+        if (selectableTasks.size() == 1) {
+            if (Preferences.getWidgetAskForTaskSelectionIfOnlyOnePreference(SelectTaskActivity.this)) {
+                showTaskSelectionDialog(selectableTasks);
+            } else {
+                Task task = selectableTasks.get(0);
+
+                Intent resultValue = new Intent();
+                resultValue.putExtra(Constants.Extras.TASK, task);
+                setResult(RESULT_OK, resultValue);
+                SelectTaskActivity.this.finish();
+            }
+        } else {
+            showTaskSelectionDialog(selectableTasks);
+        }
+    }
+
+    private List<Task> loadSelectableTasks() {
+        List<Task> selectableTasks = null;
         //Find all tasks and sort by name
         if (widgetId != null) {
             Project project = projectService.getSelectedProject(widgetId);
@@ -86,104 +103,74 @@ public class SelectTaskActivity extends SyncLockedGuiceActivity {
             selectableTasks = taskService.findAll();
         }
 
+        return selectableTasks;
+    }
 
-        if (selectableTasks == null || selectableTasks.size() == 0) {
-            showDialog(Constants.Dialog.NO_TASKS_AVAILABLE);
-        } else if (selectableTasks.size() == 1) {
-            if (Preferences.getWidgetAskForTaskSelectionIfOnlyOnePreference(SelectTaskActivity.this)) {
-                showDialog(Constants.Dialog.CHOOSE_TASK);
-            } else {
-                Task task = selectableTasks.get(0);
+    private void showTaskSelectionDialog(final List<Task> availableTasks) {
+        Collections.sort(availableTasks, new TaskByNameComparator());
+        Task selectedTask = null;
+        int selectedTaskIndex = -1;
 
-                Intent resultValue = new Intent();
-                resultValue.putExtra(Constants.Extras.TASK, task);
-                setResult(RESULT_OK, resultValue);
-                SelectTaskActivity.this.finish();
-            }
-        } else {
-            showDialog(Constants.Dialog.CHOOSE_TASK);
+        List<String> tasks = new ArrayList<String>();
+        for (Task task : availableTasks) {
+            tasks.add(task.getName());
         }
+
+        if (widgetId != null) {
+            tasks.add(getString(R.string.lbl_widget_select_new_project_task));
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.lbl_widget_title_select_task)
+                .setSingleChoiceItems(
+                        StringUtils.convertListToArray(tasks),
+                        selectedTaskIndex,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogInterface, int index) {
+                                if (index != availableTasks.size()) {
+                                    Task newSelectedTask = availableTasks.get(index);
+                                    if (!onlySelect && widgetId != null)
+                                        taskService.setSelectedTask(widgetId, newSelectedTask);
+
+                                    if (updateWidget)
+                                        widgetService.updateWidget(widgetId);
+
+                                    Intent resultValue = new Intent();
+                                    resultValue.putExtra(Constants.Extras.TASK, newSelectedTask);
+                                    setResult(RESULT_OK, resultValue);
+                                    SelectTaskActivity.this.finish();
+                                } else {
+                                    dialogInterface.dismiss();
+                                    Intent intent = new Intent(SelectTaskActivity.this, AddEditTaskActivity.class);
+                                    intent.putExtra(Constants.Extras.PROJECT, projectService.getSelectedProject(widgetId));
+                                    startActivityForResult(intent, Constants.IntentRequestCodes.ADD_TASK);
+                                }
+                            }
+                        }
+                )
+                .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    public void onCancel(DialogInterface dialogInterface) {
+                        setResult(RESULT_CANCELED);
+                        SelectTaskActivity.this.finish();
+                    }
+                });
+
+        if (enableSelectNoneOption) {
+            builder.setNeutralButton(R.string.lbl_widget_select_no_task_option, new AlertDialog.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    setResult(RESULT_OK);
+                    SelectTaskActivity.this.finish();
+                }
+            });
+        }
+        builder.create().show();
     }
 
     @Override
-    protected Dialog onCreateDialog(int id) {
-        Dialog dialog = null;
-
-        switch (id) {
-            case Constants.Dialog.NO_TASKS_AVAILABLE: {
-                AlertDialog.Builder alertNoTaskAvailable = new AlertDialog.Builder(this);
-                alertNoTaskAvailable.setMessage(R.string.msg_no_tasks_available_choose_other_project)
-                        .setCancelable(true)
-                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                removeDialog(Constants.Dialog.NO_TASKS_AVAILABLE);
-                                SelectTaskActivity.this.finish();
-                            }
-                        })
-                        .setOnCancelListener(new DialogInterface.OnCancelListener() {
-                            @Override
-                            public void onCancel(DialogInterface dialogInterface) {
-                                removeDialog(Constants.Dialog.NO_TASKS_AVAILABLE);
-                                finish();
-                            }
-                        });
-                dialog = alertNoTaskAvailable.create();
-                break;
-            }
-            case Constants.Dialog.CHOOSE_TASK: {
-                Collections.sort(selectableTasks, new TaskByNameComparator());
-                final List<Task> availableTasks = selectableTasks;
-
-                Task selectedTask = null;
-                int selectedTaskIndex = -1;
-
-                List<String> tasks = new ArrayList<String>();
-                for (Task task : availableTasks) {
-                    tasks.add(task.getName());
-                }
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle(R.string.lbl_widget_title_select_task)
-                       .setSingleChoiceItems(
-                               StringUtils.convertListToArray(tasks),
-                               selectedTaskIndex,
-                               new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialogInterface, int index) {
-                                        Task newSelectedTask = availableTasks.get(index);
-                                        if (!onlySelect && widgetId != null)
-                                            taskService.setSelectedTask(widgetId, newSelectedTask);
-
-                                        if (updateWidget)
-                                            widgetService.updateWidget(widgetId);
-
-                                        Intent resultValue = new Intent();
-                                        resultValue.putExtra(Constants.Extras.TASK, newSelectedTask);
-                                        setResult(RESULT_OK, resultValue);
-                                        SelectTaskActivity.this.finish();
-                                    }
-                               }
-                       )
-                       .setOnCancelListener(new DialogInterface.OnCancelListener() {
-                           public void onCancel(DialogInterface dialogInterface) {
-                               setResult(RESULT_CANCELED);
-                               SelectTaskActivity.this.finish();
-                           }
-                       });
-
-                if (enableSelectNoneOption) {
-                    builder.setNeutralButton(R.string.lbl_widget_select_no_task_option, new AlertDialog.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            setResult(RESULT_OK);
-                            SelectTaskActivity.this.finish();
-                        }
-                    });
-                }
-                dialog = builder.create();
-                break;
-            }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Constants.IntentRequestCodes.ADD_TASK && resultCode == RESULT_OK) {
+            showTaskSelectionDialog(loadSelectableTasks());
         }
-
-        return dialog;
     }
 }
