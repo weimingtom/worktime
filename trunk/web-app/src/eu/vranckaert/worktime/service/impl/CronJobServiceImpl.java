7aa25b287e2e228bc27742e4df363eb75eb00e36
dirkvranckaert <dirkvranckaert@5c578369-1464-59e2-a874-362b38da20de>
2013-04-03 12:34:31 +0000

    Issue 209 - Added extra info for the number of syncs (and the count of each sync status)

--- trunk/web-app/src/eu/vranckaert/worktime/service/impl/CronJobServiceImpl.java
+++ trunk/web-app/src/eu/vranckaert/worktime/service/impl/CronJobServiceImpl.java
@@ -5,17 +5,29 @@ import java.util.List;
 
 import com.google.inject.Inject;
 
+import eu.vranckaert.worktime.dao.SyncHistoryDao;
 import eu.vranckaert.worktime.model.User;
+import eu.vranckaert.worktime.model.sync.SyncHistory;
 import eu.vranckaert.worktime.security.dao.UserDao;
 import eu.vranckaert.worktime.service.CronJobService;
 import eu.vranckaert.worktime.util.DateUtil;
 
 public class CronJobServiceImpl implements CronJobService {
 	@Inject private UserDao userDao;
+	@Inject private SyncHistoryDao syncHistoryDao;
 
 	@Override
 	public List<User> findUsersRegisteredOnDay(Date date) {
 		return userDao.findUsersRegiseredBetween(
+				DateUtil.getDayWithMinimalTimeValues(date),
+				DateUtil.getDayWithMaximumTimeValues(date),
+				true
+		);
+	}
+
+	@Override
+	public List<SyncHistory> findSyncsOnDay(Date date) {
+		return syncHistoryDao.findSyncsBetween(
 				DateUtil.getDayWithMinimalTimeValues(date),
 				DateUtil.getDayWithMaximumTimeValues(date),
 				true
