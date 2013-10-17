ceb577a4e450ccc37a16540fa36ae419ba7d65bd
dirkvranckaert <dirkvranckaert@5c578369-1464-59e2-a874-362b38da20de>
2013-10-17 09:47:39 +0000

    Added extra logging...

--- web-app/src/eu/vranckaert/worktime/service/impl/SyncServiceImpl.java
+++ web-app/src/eu/vranckaert/worktime/service/impl/SyncServiceImpl.java
@@ -1004,6 +1004,9 @@ public class SyncServiceImpl implements SyncService {
 	}
 	
 	private void obscureData(TimeRegistration timeRegistration) {
+		log.info("Obscuring data for time registration with key " + timeRegistration.getKey());
+		log.info("Time registation has task " + timeRegistration.getTask().getName() + " and key " + timeRegistration.getTask().getKey());
+		log.info("Time registation has project " + timeRegistration.getTask().getProject().getName() + " and key " + timeRegistration.getTask().getProject().getKey());
 		timeRegistration.getTask().getProject().setUser(null);
 		timeRegistration.getTask().getProject().setKey(null);
 		timeRegistration.getTask().setKey(null);
@@ -1011,11 +1014,14 @@ public class SyncServiceImpl implements SyncService {
 	}
 	
 	private void obscureData(Project project) {
+		log.info("Obscuring data for project " + project.getName() + " and key " + project.getKey());
 		project.setUser(null);
 		project.setKey(null);
 	}
 	
 	private void obscureData(Task task) {
+		log.info("Obscuring data for task " + task.getName() + " and key " + task.getKey());
+		log.info("Task has project " + task.getProject().getName() + " and key " + task.getProject().getKey());
 		task.getProject().setUser(null);
 		task.getProject().setKey(null);
 		task.setKey(null);
